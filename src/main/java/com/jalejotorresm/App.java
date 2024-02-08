7d2c9c17267d0d25798fcec2b5d145d449caa26b
jalejotorresm <alejandrotorres5286@gmail.com>
2024-02-07 22:44:13 -0500

    Pruebas hechas en App

--- src/main/java/com/jalejotorresm/App.java
+++ src/main/java/com/jalejotorresm/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("\n------ Bienvenido a mi Coleccion de Miniprogramas en Java (con Maven) ------\n");
         Selector.selector();
+        System.out.println();
         System.exit(0);
     }
 }
