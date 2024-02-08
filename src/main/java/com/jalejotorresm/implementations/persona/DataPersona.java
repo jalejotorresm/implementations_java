d54ebcda31ec378fe661ed7906d9426e31d3c72a
jalejotorresm <alejandrotorres5286@gmail.com>
2024-02-07 22:49:43 -0500

    Implementacion Persona mejorada

--- src/main/java/com/jalejotorresm/implementations/persona/DataPersona.java
+++ src/main/java/com/jalejotorresm/implementations/persona/DataPersona.java
@@ -4,25 +4,39 @@ import java.util.Scanner;
 
 public class DataPersona {
     private String nombre;
-    private int edad;
+    private String edad;
 
-    public DataPersona(String nombre, int edad) {
+    public DataPersona(String nombre, String edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
 
-    @SuppressWarnings("unused")
     private void saludar() {
-        System.out.printf("\nHola, eres %s y tienes %d años de edad.\n", this.nombre, this.edad);
+        System.out.printf("\nHola, eres %s y tienes %s años de edad.\n", this.nombre, this.edad);
     }
 
     public void setData() {
     	Scanner sc = new Scanner(System.in);
-    	System.out.println("Dime tu edad: ");
-    	
-    	String input = sc.nextLine();
-    	
-    	System.out.println(input);
+        String regex = "^[a-zA-ZÀ-ÿ\u00f1\u00d1]+$";
+        System.out.println("Dime tu nombre:");
+        this.nombre = sc.nextLine();
+
+        while (!this.nombre.matches(regex)){
+            System.out.println("\nInformacion incorrecta. Dime tu nombre por favor:");
+            this.nombre = sc.nextLine();
+        }
+
+        regex = "^[0-9]+$";
+        System.out.println("\nDime tu edad:");
+        this.edad = sc.nextLine();
+
+        while (!this.edad.matches(regex)){
+            System.out.println("\nInformacion incorrecta. Dime tu edad por favor:");
+            this.edad = sc.nextLine();
+        }
+
     	sc.close();
+
+        saludar();
     }
 }
--- src/main/java/com/jalejotorresm/implementations/persona/Persona.java
+++ src/main/java/com/jalejotorresm/implementations/persona/Persona.java
@@ -2,8 +2,9 @@ package com.jalejotorresm.implementations.persona;
 
 public class Persona {
 	public static void persona() {
-		DataPersona humano = new DataPersona("", 0);
+		System.out.println("\n--- Implementacion Objetos y Metodos ---\n");
 		
+		DataPersona humano = new DataPersona("", "0");		
 		humano.setData();
 	}
 }
